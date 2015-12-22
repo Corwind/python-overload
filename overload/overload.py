34a2aa7f394a52c082e7b90076d52774ab2fd579
Guillaume Doré <dore_g@epita.fr>
2015-12-22 20:17:30 +0100

    refactor overload.py a little

--- overload/overload.py
+++ overload/overload.py
@@ -1,18 +1,22 @@
+"""
+overload.py
+
+
+"""
+
 import inspect
 
 class overload:
     functions = dict()
-
     def __init__(self, f):
         self.f = f
-        f_sig = inspect.signature(f)
-        print(f_sig)
+        funsig = inspect.signature(f)
         key = '{}_{}'.format(f.__name__, '_'.join([str(a.annotation) for a in
-            f_sig.parameters.values()]))
+            funsig.parameters.values()]))
         overload.functions[key] = f
-
+    
     def __call__(self, *args):
-        f_sig = inspect.signature(self.f)
-        key = '{}_{}'.format(self.f.__name__, '_'.join([str(a.annotation) for a in
-            f_sig.parameters.values()]))
+        funsig = inspect.signature(self.f)
+        key = '{}_{}'.format(self.f.__name__, '_'.join([str(type(a)) for a in
+            args]))
         overload.functions[key](*args)
