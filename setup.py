6aad6f859cbe66f43575114762ae7de61e65d2c1
Guillaume Doré <dore_g@epita.fr>
2015-12-22 17:43:29 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+        name='overload',
+        version='0.0.1',
+        author='Guillaume Doré',
+        author_email='guillaume@dore-net.fr',
+        url='https://github.com/Corwind/python-overload',
+        packages=['overload'],
+        classifiers=[
+            'Operating System :: GNU/Linux',
+            'Environment :: Console',
+            'Programming Language :: Python',
+            'Intended Audience :: Python enthousiasts',
+            'Topic :: IRC',
+            'License :: Beerware',
+            ]
+        )
